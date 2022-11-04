e3d2580e8f57c362103c41fc9aa750ab20b31b8a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:13:16 +0000

    84124d55bb841c0bc66fda48a47d3106917574aa
    Sync to source repo @84124d55bb841c0bc66fda48a47d3106917574aa

--- js/responsive.dataTables.min.mjs
+++ js/responsive.dataTables.min.mjs
@@ -1,1 +1,4 @@
+/*! DataTables styling wrapper for Responsive
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-dt";import DataTable from"datatables.net-responsive";export default DataTable;
