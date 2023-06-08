1e86cbdcb55928f8adc15855cf6d5880c19cb686
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-08 13:11:02 +0100

    03255a78c2147892bd975ef4ca4deb84c2fdf509 Update: Details view clickable target styling updated to be a simple arrow Update: `parent` class it automatically added by Responsive now, when viewing a row's details. This allows styling to be performed on the row for modal view as well New: Dark mode support for DataTables and Bootstrap (5.3+) styling
    Sync to source repo @03255a78c2147892bd975ef4ca4deb84c2fdf509

--- js/responsive.dataTables.min.mjs
+++ js/responsive.dataTables.min.mjs
@@ -1,4 +1,4 @@
 /*! DataTables styling wrapper for Responsive
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-dt";import Responsive from"datatables.net-responsive";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-dt";import Responsive from"datatables.net-responsive";let $=jQuery;export default DataTable;
